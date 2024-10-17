5ef5b66f1bc006d82c3ad9dca9e233290d0e49d1
Sergio Teacher <Sergio Teacher@MINIWIN>
2024-10-17 17:14:02 +0200

    El primer commit

---
+++ src/beans32/Beans32.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package beans32;
+
+/**
+ *
+ * @author Sergio Teacher
+ */
+public class Beans32 {
+
+    /**
+     * Es el método ptricipal del proyecto
+     * @param args El array de de cadena-palabras que caprura el programa
+     */
+    public static void main(String[] args) {
+        System.out.println("Iniciando el programa ...");
+        //System.out.print("El primer parámetro es: ");
+        //System.out.println( args[0]   );
+    }
+    
+}
